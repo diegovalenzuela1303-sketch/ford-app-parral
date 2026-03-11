0a14d02626b8e059af53c06d3d6fc8c8e8447966
diegovalenzuela1303-sketch <diegovalenzuela1303@gmail.com>
2026-03-11 17:16:29 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
-
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://nzmbdaljvlrfxmokiacu.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_BseMSGVh7za_8k_FASHXGw_QV4zog0D",
-  WHATSAPP_NUMBER: "526272850550",
-  ASESOR_NOMBRE: "Diego Valenzuela",
-  ASESOR_TELEFONO: "627 285 0550",
-  CIUDAD_OBJETIVO: "Parral, Chihuahua"
+
+SUPABASE_URL: "https://nzmbdaljvlrfxmokiacu.supabase.co",
+
+SUPABASE_ANON_KEY: "sb_publishable_BseMSGVh7za_8k_FASHXGw_QV4zog0D",
+
+WHATSAPP_NUMBER: "526272850550",
+
+ASESOR_NOMBRE: "Diego Valenzuela",
+
+ASESOR_TELEFONO: "627 285 0550",
+
+CIUDAD_OBJETIVO: "Parral, Chihuahua"
+
 };
